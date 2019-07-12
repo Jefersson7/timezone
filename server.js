105c845bea53906d09ee5e27fecf07c402ec511a
Jefersson <jeferssoncano@gmail.com>
2019-07-11 22:39:19 -0500

    Enabling CORS

--- server.js
+++ server.js
@@ -2,10 +2,15 @@ var express = require('express'),
   app = express(),
   port = process.env.PORT || 3000
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
+
 var routes = require('./api/routes/timeZoneRoutes')
 routes(app)
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
 })
-
